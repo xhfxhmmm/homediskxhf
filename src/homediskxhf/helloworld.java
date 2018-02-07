c9fe3502eb93e74e16b4b54d0cf70e1aa83cdb85
xhf <16281147@bjtu.edu.cn>
2018-02-07 13:27:21 +0800

    ！

--- src/homediskxhf/helloworld.java
+++ src/homediskxhf/helloworld.java
@@ -2,6 +2,6 @@ package homediskxhf;
 
 public class helloworld {
     public static void main(String []args) {
-        System.out.println("Hello World"); // ¥Ú”° Hello World
+        System.out.println("Hello World!"); // ¥Ú”° Hello World
     }
 }
